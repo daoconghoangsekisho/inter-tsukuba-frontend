fbe0e56104d4b1c0733c4af14b00f37dc74cefcb
dao_cong_hoang <dao_cong_hoang@sekisho.co.jp>
2023-09-06 17:17:31 +0900

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  const items = [{ message: 'Foo' }, { message: 'Bar' }]
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in items" v-bind:key="item.message">
+      {{ item.message }}
+    </li>
+  </ul>
+</template>
